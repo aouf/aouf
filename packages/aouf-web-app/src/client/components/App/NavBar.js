40671bc096c5d667ed4f2a55a00173239fe87441
Lucas Leroux <lucas.leroux@protonmail.com>
2019-01-27 20:22:18 +0100

    navbar added

--- packages/aouf-web-app/src/client/components/App/NavBar.js
+++ packages/aouf-web-app/src/client/components/App/NavBar.js
@@ -4,12 +4,20 @@ import { Theme } from '../../theme';
 
 const NavBar = () => (
   <nav css={Theme.NavBar}>
-    <div css={Theme.NavBarTitle}>
-      <a href="">
-        <img src={logo} alt="" />
-      </a>
-    </div>
-    <div css={Theme.NavBarMenu} />
+    <a href="" css={Theme.NavBarTitle}>
+      <img src={logo} alt="" css={Theme.NavBarTitleImage} />
+    </a>
+    <ul css={Theme.NavBarMenu}>
+      <li>
+        <a href="">La plateforme</a>
+      </li>
+      <li>
+        <a href="">Les fonctionnalités</a>
+      </li>
+      <li>
+        <a href="">Contact</a>
+      </li>
+    </ul>
   </nav>
 );
 
--- packages/aouf-web-app/src/client/theme.js
+++ packages/aouf-web-app/src/client/theme.js
@@ -13,18 +13,35 @@ export const Theme = {
   NavBar: {
     display: 'flex',
     justifyContent: 'space-between',
+    alignItems: 'center',
     height: '118px',
+    padding: '60px',
 
     backgroundColor: '#102a33',
   },
   NavBarTitle: {},
+  NavBarTitleImage: { width: '101px', height: '65px' },
   NavBarMenu: {
     display: 'flex',
     justifyContent: 'space-between',
 
-    fontFamily: 'Montserrat, sans-serif',
-    fontWeight: 600,
-    color: '#fd7b66',
-    textTransform: 'uppercase',
+    li: {
+      margin: '35px',
+      '&:first-child': {
+        margin: '35px 35px 35px 0',
+      },
+      '&:last-child': {
+        margin: '35px 0 35px 35px',
+      },
+      listStyleType: 'none',
+      a: {
+        fontFamily: 'Montserrat, sans-serif',
+        fontSize: '18px',
+        fontWeight: 600,
+        color: '#fd7b66',
+        textDecoration: 'none',
+        textTransform: 'uppercase',
+      },
+    },
   },
 };
