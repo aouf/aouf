82a0661d20d62277907e18f841bdb8ec9fb13c3d
Lucas Leroux <lucas.leroux@protonmail.com>
2019-01-27 01:06:28 +0100

    [fix] NavBar import in Layout

--- packages/aouf-web-app/src/client/components/App/Layout.js
+++ packages/aouf-web-app/src/client/components/App/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 // import logo from '../assets/logo-big.png';
 import { Global } from '@emotion/core';
+import NavBar from './NavBar';
+
 import { GlobalSettings } from '../../theme';
 
 const Layout = ({ children }) => (
   <>
     <Global styles={GlobalSettings} />
-    <nav />
+    <NavBar />
     {children}
     <footer />
   </>
